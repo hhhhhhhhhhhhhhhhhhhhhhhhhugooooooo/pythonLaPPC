ae70816018335743f05e85e6866dd49895425cf7
hhhhhhhhhhhhhhhhhhhhhhhhhugooooooo <122105420+hhhhhhhhhhhhhhhhhhhhhhhhhugooooooo@users.noreply.github.com>
2023-01-29 20:15:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import pickle
 # Create a Python script that simulates an energy-producing and consuming home
 class Home(multiprocessing.Process):
     def __init__(self, production_rate, consumption_rate, trade_policy,socket_number, queue):
-		self.a=production_rate
+        self.a=production_rate
         self.production_rate = production_rate
         self.consumption_rate = consumption_rate
         self.trade_policy = trade_policy
@@ -19,18 +19,19 @@ class Home(multiprocessing.Process):
         self.queue=queue
         
     def receiveData(self):
-		self.socket_number.recv(pickle.loads)
+        self.socket_number.recv(pickle.loads)
     def calculate_surplus(self):
         self.surplus = self.surplus+self.production_rate - self.consumption_rate
         
     def calculate_production_rate(self):
-		receiveData()
-		production_rate=a*(2*ensoleillement)
-		pass
+        self.receiveData()
+        self.production_rate=self.a*(2*ensoleillement)
+        pass
 		
-	def calculate_consumption_rate(self):
-		receiveData()
-		consuption_rate=consumption_rate+(22-temperature)*0.5
+        
+    def calculate_consumption_rate(self):
+        self.receiveData()
+        self.consuption_rate=self.consumption_rate+(22-temperature)*0.5
         
     def trade_energy(self):
         if self.trade_policy == "always_give":
@@ -38,60 +39,58 @@ class Home(multiprocessing.Process):
             # give consumption_rate/10
             self.queue.put("Anyone in need ?")
             time.sleep(1)
-            if (self.queue.get()=="Anyone in need ?" and self.surplus<production_rate/10):
-				self.queue.put("I am in need, my pid is "+self.getpid())
-			time.sleep(1)
-			if ((self.queue.get())==str.startswith("I am in need, my pid is ") and self.surplus>self.consumption_rate/5):
-				num_string=(self.queue.get()).split("is ")[-1].strip()   #get pid of the process that sent the message
-				num=int(num_string)
-				self.queue.put(num+", here is some energy, "+consumption_rate/10)				#mutex lock à mettre ici pour protéger le changement de la valeur surplus
-				self.surplus=self.surplus-consumption_rate/10
+            if (self.queue.get()=="Anyone in need ?" and self.surplus<self.production_rate/10):
+                self.queue.put("I am in need, my pid is "+self.getpid())
+            time.sleep(1)
+            if ((self.queue.get())==str.startswith("I am in need, my pid is ") and self.surplus>self.consumption_rate/5):
+                num_string=(self.queue.get()).split("is ")[-1].strip()   #get pid of the process that sent the message
+                num=int(num_string)
+                self.queue.put(num+", here is some energy, "+self.consumption_rate/10)				#mutex lock à mettre ici pour protéger le changement de la valeur surplus
+                self.surplus=self.surplus-self.consumption_rate/10
             # calls queue to listen for givers (if surplus<production_rate/10)
 				#if no givers use socket to buy
             pass
         elif self.trade_policy == "always_sell":
             # sell surplus energy to the market
             # sell consumption_rate/10
-            if (self.surplus>consumption_rate/5):
-				sellToMarket(consumption_rate/10)
-            if (self.queue.get()=="Anyone in need ?" and self.surplus<production_rate/10):
-				self.queue.put("I am in need, my pid is "+self.getpid())
+            if (self.surplus>self.consumption_rate/5):
+                self.sellToMarket(self.consumption_rate/10)
+            if (self.queue.get()=="Anyone in need ?" and self.surplus<self.production_rate/10):
+                self.queue.put("I am in need, my pid is "+self.getpid())
             # does not call queue for takers (as it always sells)
 				#therefore sell using socket
             # calls queue to listen for givers(if surplus<prodution_rate/10)
 				#if no givers use socket to buy
             pass
-        else:  self.trade_policy == "sell_if_no_takers"
-            # check if other homes want surplus energy
+        else: 
             self.queue.put("Anyone in need ?")
             time.sleep(1)
-            if (self.queue.get()=="Anyone in need ?" and self.surplus<production_rate/10):
-				self.queue.put("I am in need, my pid is "+self.getpid())
-			time.sleep(1)
-			if ((self.queue.get())==str.startswith("I am in need, my pid is ") and self.surplus>self.consumption_rate/5):
-				num_string=(self.queue.get()).split("is ")[-1].strip()   #get pid of the process that sent the message
-				num=int(num_string)
-				self.queue.put(num+", here is some energy, "+consumption_rate/10)
-			else :
-				sellToMarket(consumption_rate/10)
+            if (self.queue.get()=="Anyone in need ?" and self.surplus<self.production_rate/10):
+                self.queue.put("I am in need, my pid is "+self.getpid())
+                time.sleep(1)
+            if ((self.queue.get())==str.startswith("I am in need, my pid is ") and self.surplus>self.consumption_rate/5):
+                num_string=(self.queue.get()).split("is ")[-1].strip()   #get pid of the process that sent the message
+                num=int(num_string)
+                self.queue.put(num+", here is some energy, "+self.consumption_rate/10)
+            else :
+                self.sellToMarket(self.consumption_rate/10)
             # if not, sell to the market
             # trade consumption_rate/10
             # calls queue to ask for takers (if no takers sell)
 				#therefore sell using socket
             # calls queue to listen for givers (if surplus<production_rate/10)
 				#if no givers use socket to buy
-            pass
+                pass
             
-	def sellToMarket(amountSold):
+    def sellToMarket(amountSold):
 		#sells some energy to the market
-		
-		pass
-    
-	def run:
-		while True : 
-			if (surplus > consumption_rate/5 or surplus<production_rate/10):
-				trade_energy()
-			calculate_surplus()
+        pass
+
+    def run(self):
+        while True : 
+            if (self.surplus > self.consumption_rate/5 or self.surplus<self.production_rate/10):
+                self.trade_energy()
+            self.calculate_surplus()
 			
 		
 
@@ -134,7 +133,7 @@ class Market(multiprocessing.Process):
             #temp.temperature = 22
         print("Received SIGUSR2. Outputting data to output2.txt...")        
 
-    def update_critical_data(self, transaction_amount, energy_sold,internal_factors,externall_factors):
+    def update_critical_data(self, transaction_amount, energy_sold,internal_factors,external_factors):
         # acquire lock to prevent multiple transactions from happening simultaneously
         self.transactions_lock.acquire()
         self.stock=self.stock-energy_sold
@@ -142,12 +141,12 @@ class Market(multiprocessing.Process):
         self.transactions_lock.release()
         
     def run(self):
-		threadh1=threading.Thread(target=self.communicate_with_h1)
-		threadh2=threading.Thread(target=self.communicate_with_h2)
-		threadh3=threading.Thread(target=self.communicate_with_h3)
-		threadh1.start()
-		threadh2.start()
-		threadh3.start()
+        threadh1=threading.Thread(target=self.communicate_with_h1)
+        threadh2=threading.Thread(target=self.communicate_with_h2)
+        threadh3=threading.Thread(target=self.communicate_with_h3)
+        threadh1.start()
+        threadh2.start()
+        threadh3.start()
 		
         print('salut')
         t=External()
@@ -158,15 +157,15 @@ class Market(multiprocessing.Process):
         while True:
             time.sleep(1)
             pass
-	def communicate_with_h1(self):
-		self.sock1.send(pickle.dumps(price), pickle.dumps(ensoleillement),pickle.dumps(temperature))
-		self.sock1.recv(pickle.loads)
-	def communicate_with_h2(self):
-		self.sock2.send(pickle.dumps(price), pickle.dumps(ensoleillement),pickle.dumps(temperature))
-		self.sock2.recv(pickle.loads)
-	def communicate_with_h3(self):
-		self.sock3.send(pickle.dumps(price), pickle.dumps(ensoleillement),pickle.dumps(temperature))
-		self.sock3.recv(pickle.loads)
+    def communicate_with_h1(self):
+        self.sock1.send(pickle.dumps(self.price,self.ensoleillement,self.temperature))   #bonne syntaxe à utiliser, même si je ne sais pas pourquoi il y a un problème à la ligne 162
+        [price_given,ensol_given,temp_given]=pickle.loads(self.sock1.recv(10000))
+    def communicate_with_h2(self):
+        self.sock2.send(pickle.dumps(self.price), pickle.dumps(ensoleillement),pickle.dumps(temperature))
+        self.sock2.recv(pickle.loads)
+    def communicate_with_h3(self):
+        self.sock3.send(pickle.dumps(self.price), pickle.dumps(ensoleillement),pickle.dumps(temperature))
+        self.sock3.recv(pickle.loads)
 	
 
 
@@ -237,36 +236,36 @@ class External(multiprocessing.Process):
  
                
 if __name__=='__main__':
-	temperature=multiprocessing.Value('d',22)
-	temp_change=multiprocessing.Value('i',2)
-	ensoleillement=multiprocessing.Value('d',0.5)
-	upDown=multiprocessing.Value('i',-1)
-	duration=multiprocessing.Value('i',5)
+    temperature=multiprocessing.Value('d',22)
+    temp_change=multiprocessing.Value('i',2)
+    ensoleillement=multiprocessing.Value('d',0.5)
+    upDown=multiprocessing.Value('i',-1)
+    duration=multiprocessing.Value('i',5)
 	
-	
-	sock1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock3 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
     sock1.bind(('localhost', 10000))
     _, port1 = sock1.getsockname()
     sock1.listen()
     conn1, _ = sock1.accept()
     
-    sock2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock2.bind(('localhost', 20000))
     _, port2 = sock2.getsockname()
     sock2.listen()
     conn2, _ = sock2.accept()
     
     
-    sock3 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock3.bind(('localhost', 30000))
     _, port3 = sock3.getsockname()
-    sock3listen()
+    sock3.listen()
     conn3, _ = sock3.accept()
+    
+    queue=multiprocessing.Queue()
 	
-	queue=multiprocessing.Queue()
 	
-	
-    m = Market(10,temperature,temp_change,ensoleillement,upDown,duration,sconn1,conn2,conn3)
+    m = Market(10,temperature,temp_change,ensoleillement,upDown,duration,conn1,conn2,conn3)
     p=Weather(temperature,temp_change,ensoleillement,upDown,duration)
     h1=Home(10,8,"always give",conn1,queue)
     h2=Home(10,8,"always sell",conn2,queue)
@@ -281,5 +280,3 @@ if __name__=='__main__':
     h1.join()
     h2.join()
     h3.join()
-    
-
